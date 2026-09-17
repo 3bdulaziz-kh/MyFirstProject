eac6ac99dc2dc129c7fad39a38877fb97f8259a4
3bdulazi-kh <445050144@std.psau.edu.sa>
2026-09-17 23:54:16 +0300

    Initial commit

--- HelloWorldd.java
+++ HelloWorldd.java
@@ -1,6 +1,6 @@
 }public class HelloWorldd {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello, 3z");
         System.out.println("Welcome to my awesome Git project!");
     }
 }
