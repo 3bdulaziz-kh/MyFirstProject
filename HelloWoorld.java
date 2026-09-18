6215eb91c21bed2a8741bce9f9eede4664f0676a
3bdulazi-kh <445050144@std.psau.edu.sa>
2026-09-18 16:24:12 +0300

    Add greeting feature

--- HelloWoorld.java
+++ HelloWoorld.java
@@ -1,6 +1,7 @@
-}public class HelloWorldd {
+public class HelloWoorld {
     public static void main(String[] args) {
-        System.out.println("Hello, 3z");
+        System.out.println("Hello, World!");
+        
         System.out.println("Welcome to my awesome Git project!");
     }
 }
